679b1df34aff159517e72ebcf9dfef44bc7fedc0
Badhrinarayanan <badhrinarayanan.m@siemens.com>
2022-05-31 15:37:13 +0530

    tshirts failing

--- tshirts.js
+++ tshirts.js
@@ -1,16 +1,15 @@
-const {expect} = require('chai');
-
+const { expect } = require('chai');
 function size(cms) {
-    if (cms < 38) {
-        return 'S';
-    } else if (cms > 38 && cms < 42) {
-        return 'M';
-    } else {
-        return 'L';
-    }
+	if (cms < 38) {
+		return 'S';
+	} else if (cms > 38 && cms < 42) {
+		return 'M';
+	} else {
+		return 'L';
+	}
 }
 
-expect(size(37)).equals('S');
+expect(size(38)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
 console.log('All is well (maybe!)');
