d78c6f68c8bb052e2ed1f1d24b0c623484737c9e
Badhrinarayanan <badhrinarayanan.m@siemens.com>
2022-06-08 22:19:01 +0530

    updated for pass test

--- misaligned.js
+++ misaligned.js
@@ -1,29 +1,79 @@
 const { expect } = require('chai');
 const majorColors = ['White', 'Red', 'Black', 'Yellow', 'Violet'];
 const minorColors = ['Blue', 'Orange', 'Green', 'Brown', 'Slate'];
-function print_color_map(colorMap) {
-	for (const colorNumber in colorMap) {
-		console.log(
-			`${colorNumber} | ${colorMap[colorNumber].majorColor} | ${colorMap[colorNumber].minorColor}`
+let colorMap = [];
+
+class ColorMapper {
+	static majorColorPadEnd = 0;
+	static minorColorPadEnd = 0;
+	static colorNumberPadEnd = 0;
+	constructor(colorNumber, majorColor, minorColor) {
+		this.colorNumber = colorNumber;
+		this.majorColor = majorColor;
+		this.minorColor = minorColor;
+		ColorMapper.colorNumberPadEnd = ColorMapper.getPadEnd(
+			this.colorNumber.toString(),
+			ColorMapper.colorNumberPadEnd
+		);
+		ColorMapper.majorColorPadEnd = ColorMapper.getPadEnd(
+			this.majorColor.toString(),
+			ColorMapper.majorColorPadEnd
+		);
+		ColorMapper.minorColorPadEnd = ColorMapper.getPadEnd(
+			this.minorColor.toString(),
+			ColorMapper.minorColorPadEnd
 		);
 	}
+	static getPadEnd(string, maxStringLen) {
+		if (string.length > maxStringLen) {
+			maxStringLen = string.length;
+		}
+		return maxStringLen;
+	}
+}
+function print_color_map(colorMap) {
+	colorMap.forEach((colorObj) => {
+		colourFormatter(
+			colorObj.colorNumber,
+			ColorMapper.colorNumberPadEnd,
+			colorObj.majorColor,
+			ColorMapper.majorColorPadEnd,
+			colorObj.minorColor,
+			ColorMapper.minorColorPadEnd
+		);
+	});
 	return Object.keys(colorMap).length;
 }
 
 function findColorMap(majorColors, minorColors) {
-	colorMap = {};
 	for (let i = 0; i < majorColors.length; i++) {
 		for (let j = 0; j < minorColors.length; j++) {
-			colorMap[i * 5 + j + 1] = {
-				majorColor: majorColors[i],
-				minorColor: minorColors[j],
-			};
+			colorMap.push(
+				new ColorMapper(i * 5 + j + 1, majorColors[i], minorColors[j])
+			);
 		}
 	}
 	return colorMap;
 }
 
+function colourFormatter(
+	colorNumber,
+	colorNumberPadEnd,
+	majorColor,
+	majorColorPadEnd,
+	minorColor,
+	minorColorPadEnd
+) {
+	console.log(
+		`${colorNumber.toString().padEnd(colorNumberPadEnd, ' ')} | ${majorColor
+			.toString()
+			.padEnd(majorColorPadEnd, ' ')} | ${minorColor
+			.toString()
+			.padEnd(minorColorPadEnd, ' ')}`
+	);
+}
+
 expect(print_color_map(findColorMap(majorColors, minorColors))).equals(
-	majorColors.length * minorColors.length + 1
+	majorColors.length * minorColors.length
 );
 console.log('All is well (maybe!)');
--- tshirts.js
+++ tshirts.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 function size(cms) {
-	if (cms < 38) {
+	if (cms <= 38) {
 		return 'S';
 	} else if (cms > 38 && cms < 42) {
 		return 'M';
@@ -9,7 +9,8 @@ function size(cms) {
 	}
 }
 
+expect(size(37)).equals('S');
 expect(size(38)).equals('S');
-expect(size(40)).equals('L');
-expect(size(43)).equals('M');
+expect(size(40)).equals('M');
+expect(size(43)).equals('L');
 console.log('All is well (maybe!)');
